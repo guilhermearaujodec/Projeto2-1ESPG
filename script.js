695674c1a84bba562b1c84e8c9f7a8ae98146663
Guilherme <gui_araujo.carvalho@hotmail.com>
2024-04-05 09:29:10 -0300

    Criando o Script

---
+++ script.js
@@ -0,0 +1,5 @@
+function Calcular(){ //Aplicando função com DOM
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    document.getElementById('resultado').innerHTML =num1 + num2;
+}
